336095977fb3280e82c4099b596ce3aba0023a42
r0zmarin1 <IRozina2006@gmail.com>
2020-11-05 19:15:55 +1000

    hyhhhhhhbj

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import sys
 W = 800
 H = 600
 collide = False
+n = 0
 
 # квадрат
 rect_size = w, h = 70, 70
@@ -12,30 +13,58 @@ rect_pos = ((W - w) // 2, (H - h) // 2)
 circle_radius = 35
 circle_pos = (0, 0)
 # цвет
-p = (221, 154, 207)
-pp = (231, 102, 208)
-pp1 = (238, 0, 209)
+p = (221, 154, 207, 180)
+pp = (231, 102, 208, 180)
+pp1 = (238, 0, 209, 180)
 BG = (180, 0, 157)
 
 pygame.init()
-pygame.display.set_caption('fuyu no hanashi')
+pygame.display.set_caption('yarichin B-club')
 screen = pygame.display.set_mode((W, H))
+font = pygame.font.Font(None, 32)
+# создаем поверхность размером в 2 раза больше радиуса круга и вкл. альфа-канал
+surface = pygame.Surface((circle_radius * 2, circle_radius * 2), pygame.SRCALPHA)
+# на созданной поверхности рисуем круг pp1 цвета
+pygame.draw.circle(surface, pp1, (circle_radius, circle_radius), circle_radius)
+# находим рект у поверхности
+rect1 = surface.get_rect()
+
+clock = pygame.time.Clock()
+FPS = 660
+speed_x, speed_y = 1, 1
+
+ball = pygame.image.load('ball.png')
+ball_rect = ball.get_rect()
+
 
 while True:
+    clock.tick(FPS)
     for e in pygame.event.get():
         if e.type == pygame.QUIT:
             sys.exit(0)
         elif e.type == pygame.MOUSEMOTION:
-            circle_pos = e.pos
+            # circle_pos = e.pos
+            rect1.center = e.pos
     
-    screen.fill(BG)
+    ball_rect = ball_rect.move(speed_x, speed_y)
+    if ball_rect.left < 0 or ball_rect.right > W:
+        speed_x = -speed_x
+    if ball_rect.top < 0 or ball_rect.bottom > H:
+        speed_y = -speed_y
 
-    rect1 = pygame.draw.circle(screen, pp1, circle_pos, circle_radius)
+    screen.fill(BG)
+    COLOR = p if collide is True else pp
+    # rect1 = pygame.draw.circle(screen, pp1, circle_pos, circle_radius)
     rect2 = pygame.draw.rect(screen, p if collide is True else pp, (rect_pos, rect_size))
+    screen.blit(surface, rect1)
 
     if rect1.colliderect(rect2):
         collide = True
+        if COLOR == pp:
+            n += 1
     else:
         collide = False
 
+    screen.blit(ball, ball_rect)
+    screen.blit(font.render(str(n), 1, p), (10, 10))
     pygame.display.update()
